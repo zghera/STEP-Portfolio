6bc18346c317e9049fca09629c6ae02f5fead1f2
Zach Ghera <zpghera00@gmail.com>
2020-06-01 17:58:15 -0400

    Play around with example code.

--- walkthroughs/week-3-server/examples/random-quotes/src/main/webapp/script.js
+++ walkthroughs/week-3-server/examples/random-quotes/src/main/webapp/script.js
@@ -69,8 +69,13 @@ function getRandomQuoteUsingPromiseChain() {
  * whichever syntax makes the most sense to you.
  */
 function getRandomQuoteUsingArrowFunctions() {
-  fetch('/random-quote').then(response => response.text()).then((quote) => {
+  fetch('/random-quote')
+  .then(response => response.text())
+  .then(quote => {
     document.getElementById('quote-container').innerText = quote;
+  })
+  .catch(err => {
+    console.log("Error: " + err);
   });
 }
 
