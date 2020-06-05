c42b5eb452909d1aac631a0bea30310865ccf0f4
Zach Ghera <zpghera00@gmail.com>
2020-06-05 18:06:00 -0400

    Initial implementatiof deleting all keys in datastore.

---
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteCommentsServlet.java
@@ -0,0 +1,59 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.getKey;
+import com.google.appengine.api.datastore.Key;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet that enters new comments into the Datastore. */
+@WebServlet("/delete-comments")
+public class DeleteCommentsServlet extends HttpServlet {
+  private static DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+  /**
+   * This Method handles POST requests corresponding to a new comment and creates a new Entity for
+   * that comment in the Google Cloud Datastore.
+   *
+   * <p>The POST request also results in a re-direct back to the original server-dev page.
+   * TODO(Issue #15): Do verfification on a new comment before adding it to the comments list.
+   *
+   * @param request The <code>HttpServletRequest</code> for the POST request.
+   * @param response The <code>HttpServletResponse</code> for the POST request.
+   * @return None. A Entity of the Comment kind is created and upserted to the Datastore.
+   */
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("Comment");
+    PreparedQuery results = datastore.prepare(query);
+
+    List<Key> entityKeys = new ArrayList();
+    for (Entity commentEntity : results.asIterable()) {
+      entityKeys.append(commentEntity.getKey())
+    }
+    for (Key key : entityKeys) {
+      datastore.delete(key)
+    }
+
+    response.sendRedirect("/pages/server-dev.html");
+  }
+}
